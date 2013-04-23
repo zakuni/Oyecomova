b458cdbba2275dc084d1f533c350e11654aea2fa
zakuni <kunio038@gmail.com>
2013-04-23 22:26:43 +0900

    add oyecomova.js

---
+++ oyecomova.js
@@ -0,0 +1,18 @@
+$(function() {
+    $('html').keyup(function(e) {
+        switch(e.which) {
+        case 38: // up cursor key
+            $('h1').animate({
+                fontSize: '70'
+            }, 1000, function() {
+            });
+            break;
+        case 40: // down cursor key
+            $('h1').animate({
+                fontSize: '200'
+            }, 1000, function() {
+            });
+            break;
+        }
+    });
+});
