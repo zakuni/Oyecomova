ca0cb71369b9b55c8fdc300752ab7413b9c57df4
zakuni <kunio038@gmail.com>
2013-04-24 23:26:05 +0900

    show and hide not h1 but section

--- oyecomova.js
+++ oyecomova.js
@@ -6,14 +6,14 @@ $(function() {
                 fontSize: '70'
             }, 1000, function() {
             });
-            $('h1').show();
+            $('section').show();
             break;
         case 40: // down cursor key
             $('h1').animate({
                 fontSize: '200'
             }, 1000, function() {
-                $('h1').hide();
-                $('h1#current').show();
+                $('section').hide();
+                $('section#current').show();
             });
             break;
         }
