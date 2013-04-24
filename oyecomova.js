1f19e3b3cffe6e5c52462a642ec62fb40c14c1e5
zakuni <kunio038@gmail.com>
2013-04-24 23:24:21 +0900

    show current h1 only

--- oyecomova.js
+++ oyecomova.js
@@ -6,11 +6,14 @@ $(function() {
                 fontSize: '70'
             }, 1000, function() {
             });
+            $('h1').show();
             break;
         case 40: // down cursor key
             $('h1').animate({
                 fontSize: '200'
             }, 1000, function() {
+                $('h1').hide();
+                $('h1#current').show();
             });
             break;
         }
